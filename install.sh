fbc4ecc78b22cff393feed819460e19dc7ac3b88
Fergal Moran <fergal.moran@gmail.com>
2017-06-15 20:21:27 +0100

    Added nerdfonts

--- install.sh
+++ install.sh
@@ -75,3 +75,8 @@ vim +BundleInstall +qall
 
 git config --global user.email "fergal.moran@gmail.com"
 git config --global user.name "Fergal Moran"
+
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+
